f25a09d532a5bd3960fc7a9cbbe1f6be41d237a5
Nicholas Meyers <nicholas.meyers@hotmail.comxit>
2025-07-29 18:40:31 +0200

    chore: add logging

--- src/main/java/be/nicholasmeyers/guardiangatewaydashboardapi/log/usecase/CreateLogUseCase.java
+++ src/main/java/be/nicholasmeyers/guardiangatewaydashboardapi/log/usecase/CreateLogUseCase.java
@@ -1,11 +1,14 @@
 package be.nicholasmeyers.guardiangatewaydashboardapi.log.usecase;
 
 import be.nicholasmeyers.guardiangatewaydashboardapi.log.domain.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateLogUseCase {
 
+    private static final Logger log = LoggerFactory.getLogger(CreateLogUseCase.class);
     private final LogRepository logRepository;
 
     public CreateLogUseCase(LogRepository logRepository) {
@@ -13,6 +16,7 @@ public class CreateLogUseCase {
     }
 
     public void createLog(LogCreateRequest createLogRequest) {
+        log.info("Create log: {}", createLogRequest);
         logRepository.save(LogFactory.createLog(createLogRequest));
     }
 }
